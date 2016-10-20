7d993af17b7dd63eee899202cf722da4179e62a8
fiiidget <shackney@pratt.edu>
2016-10-20 10:37:37 -0400

    update dates
    gets the first date online

--- yearsort.py
+++ yearsort.py
@@ -3,7 +3,7 @@ import re
 
 all_years = [] # this will eventually hold the dictionaries of each year.
 
-with open("text1965gemini7.html.txt", "r") as f:
+with open("textbreconbeacons.html.txt", "r", encoding="utf-8") as f:
     # a sample text, to be tested with a random sampling before expanding
     # into looping through all the texts.
     text = f.readlines()
@@ -15,6 +15,8 @@ for line in text:
         the_year = match.group()
         print ("Match found: ", the_year)
         #proof of concept that this search can find the year
+        break #it stops looking once it finds the first one
     else:
-        print ("No match")
+        continue
         #general testing for other four-digit numbers in the text
+        #it keeps going line to line until it does find one.
