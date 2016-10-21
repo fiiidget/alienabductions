9569cd184176858ff53f88a24a4106f66abf7c89
fiiidget <shackney@pratt.edu>
2016-10-21 18:00:21 -0400

    ta-da! 🎇🎈🎊
    the year-sorting code works, is commented. List of files missed by the
    regex, to check by hand.

--- yearsort.py
+++ yearsort.py
@@ -3,6 +3,7 @@ import re
 import os
 import sys
 
+
 def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
     enc = file.encoding
     if enc == 'UTF-8':
@@ -10,37 +11,39 @@ def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
     else:
         f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
         print(*map(f, objects), sep=sep, end=end, file=file)
+        # this is a wrapper function that helps with printing unicode characters. It's not super necessary when writing to files, but prevent unicode-related errors when printing test runs. Thanks to Jelle Fresen on Stackoverflow, via Eugene Rutigliano.
 
-all_years = {} # this will eventually hold the dictionaries of each year.
-found = (0)
-notfound = []
+all_years = {} # this is the dictionary that will eventually hold the lists of texts from each year.
 indir = "C:\\Users\\Sarah Hackney\\Documents\\GitHub\\alienabductions\\UFOCasebook_text"
 for root, dirs, filenames in os.walk(indir):
     for f in filenames:
 
         with open(indir+"\\"+str(f), "r", encoding="utf-8") as thisfile:
-            # this loops through all the files in the folder listed above one by one
+            # this loops through all the files in the above folder listed above one by one
             clean = thisfile.read().replace('\n', '')
-            # for body in clean:
+            # this gets rid of the linebreaks in the docs, and allows them to be read as a whole, rather than line-by-line.
 
-            match = re.search("\d\d\d\d", clean) # possibly add a leading and trailing space? see what happens in sampling.
+            match = re.search("\d\d\d\d", clean)
+            # a super basic regex looking for a 4-digit number.
 
             if match:
                 the_year = match.group()
-                print ("Match found: ", the_year)
-                #proof of concept that this search can find the year
-                found = (found + 1) #counts how many files have a year in them
-                all_years[the_year] = clean
-                #this adds the text as the key to the dictionary. As of right now, it creates duplicates of keys with the same years
-                # break #it stops looking once it finds the first one
-
-
+                # this search is not greedy and stops looking for 4-digit numbers after it finds one. With some exceptions, which will be corrected by hand, this will be the year of the event, because each text file begins with it's title, which contains the year in which it occured.
+                if the_year in all_years:
+                    all_years[the_year].append(clean)
+                else:
+                    all_years[the_year] = [clean]
+                # This looks for a dictonary key for each year in the all_years dictionary, and adds the document's text if it finds one. If not, it creates a new key for that year, and adds a list containing the text as the value.
 
             else:
+                uprint(str(f))
+                #print the names of files that don't have a 4-digit number in them, for me to refer to manually and correct.
                 continue
 
-            #general testing for other four-digit numbers in the text
-            #it keeps going line to line until it does find one.
-uprint(found, all_years)
 
-#maybe do it as is and consolidate in openrefine. maybe? Next step is to write to csv.
+write = csv.writer(open('storybyyear.csv', 'w'))
+
+for key, value in all_years.items():
+     write.writerow([key, value])
+
+# write all of this to a csv file, which I will then clean up some with openrefine.
